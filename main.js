a3a0a8bdc829736b765c78f0a1821aded8c64648
tomriddle <aaarshdwivedi244475@gmail.com>
2023-01-10 02:19:19 +0530

    Basic Application Ready

---
+++ main.js
@@ -0,0 +1,42 @@
+
+// all imports are defined below
+
+// importing path
+const path = require('path');
+
+// importing the splashMainWindow for splash application
+const {SplashMainWindow} = require('components');
+
+// importing the toolset for splash application
+const {Toolset} = require('toolset');
+
+// ----------END OF IMPORTS---------------------
+
+
+
+// all global variables are mentioned below
+
+var indexHtmlFilePath = path.resolve('./components/html/index.html')
+
+// creating splashToolset object
+const splashToolset = new Toolset(indexHtmlFilePath);
+
+// creating splashMainWindow object
+const mainWindow = new SplashMainWindow();
+
+// -----------END OF GLOBAL VARIABLES--------------------
+
+indexHtmlFilePath = splashToolset.config.htmlFile;
+
+// setting transparent main window
+mainWindow.mainWindowConfig = {...mainWindow.mainWindowConfig, transparent: true};
+
+// starting the application
+mainWindow.start()
+
+// loading html file
+mainWindow.loadHtmlFile(indexHtmlFilePath);   
+
+// enabling shortcuts for the main window
+mainWindow.enableShortcuts();
+
