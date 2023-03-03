dec8229132f329fd2fcc48c21aba3f72ed8eb740
tom-avilius <aaarshdwivedi244475@gmail.com>
2023-03-04 00:23:51 +0530

    added styling to vertical navbar.

---
+++ docs/assets/js/docs.js
@@ -0,0 +1,29 @@
+
+// constants and variables declarations
+
+// vertical navbar items
+const vnav1 = document.getElementById('vnav1');
+const vnav2 = document.getElementById('vnav2');
+
+// ---------------------END OF DECLARATIONS-------------------------
+
+
+
+// all event listeners are attached below
+
+// adding on_click event listener to vnav1 to simulate click behavior
+vnav1.addEventListener('click', (event) => {
+
+    vnav1.classList.add('vnav-item-click');
+
+    vnav2.classList.remove('vnav-item-click');
+});
+
+
+// adding on_click event listener to vnav2 to simulate click behavior
+vnav2.addEventListener('click', (event) => {
+
+    vnav2.classList.add('vnav-item-click');
+
+    vnav1.classList.remove('vnav-item-click');
+});
