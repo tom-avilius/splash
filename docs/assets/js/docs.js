86466bd24a30318879edce37b885a55bd766a537
tom-avilius <aaarshdwivedi244475@gmail.com>
2023-03-04 16:32:50 +0530

    Added behaviour to vnav3 item.

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -4,6 +4,7 @@
 // vertical navbar items
 const vnav1 = document.getElementById('vnav1');
 const vnav2 = document.getElementById('vnav2');
+const vnav3 = document.getElementById('vnav3');
 
 // ---------------------END OF DECLARATIONS-------------------------
 
@@ -11,19 +12,30 @@ const vnav2 = document.getElementById('vnav2');
 
 // all event listeners are attached below
 
-// adding on_click event listener to vnav1 to simulate click behavior
+// adding click event listener to vnav1 to simulate click behavior
 vnav1.addEventListener('click', (event) => {
 
     vnav1.classList.add('vnav-item-click');
 
     vnav2.classList.remove('vnav-item-click');
+    vnav3.classList.remove('vnav-item-click');
 });
 
 
-// adding on_click event listener to vnav2 to simulate click behavior
+// adding click event listener to vnav2 to simulate click behavior
 vnav2.addEventListener('click', (event) => {
 
     vnav2.classList.add('vnav-item-click');
 
     vnav1.classList.remove('vnav-item-click');
+    vnav3.classList.remove('vnav-item-click');
 });
+
+// adding click event listener to vnav3 to simulate click behaviour
+vnav3.addEventListener('click', (event) => {
+
+    vnav3.classList.add('vnav-item-click');
+
+    vnav1.classList.remove('vnav-item-click');
+    vnav2.classList.remove('vnav-item-click');
+})
